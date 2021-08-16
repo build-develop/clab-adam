702b0203cf4fd3f0fc403486d6b0d260e538d47b
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-16 08:34:19 +0530

    2nd one added

--- 1.c
+++ 1.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main(){
-    printf("testing");
+    
+for(int i=5;i>0;i--){
+    for(int j=i;j>0;j--){
+        printf("*");
+    } printf("\n");
+}
+
     return 0;
 }
---
+++ 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{   int num1, num2, flag, i, j;
+    printf("Enter two range:");
+  scanf("%d %d", &num1, &num2); 
+    printf("prime numbers are: \n");
+   for(i=num1+1; i<num2; ++i)
+   {      flag=0;
+      for(j=2; j<=i/2; ++j)
+      {         if(i%j==0)
+         { flag=1;  break; }
+      }
+      if(flag==0)
+         printf("%d\n",i);
+  }
+  return 0;
+}
