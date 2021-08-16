788f899317f2c9434699bc2d06b89bd02a525136
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-16 08:35:50 +0530

    2 updated

--- 2.c
+++ 2.c
@@ -8,7 +8,7 @@ int main()
    {      flag=0;
       for(j=2; j<=i/2; ++j)
       {         if(i%j==0)
-         { flag=1;  break; }
+         { flag=1;   }
       }
       if(flag==0)
          printf("%d\n",i);
