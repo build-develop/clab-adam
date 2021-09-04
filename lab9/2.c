f45e6d0bb099fa5bc4d1d87912a98a2be3a01695
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-09-05 00:06:59 +0530

    9.3

--- lab9/2.c
+++ lab9/2.c
@@ -12,27 +12,17 @@ void main()
     int arr[size];
     printf("Enter elements in array : ");
     for (i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    int temp = 0;
+    {       scanf("%d", &arr[i]);
+    }    int temp = 0;
     int x = 0;
-
     for (int k = 0; k < size; k++)
-    {
-        x = 0;
-        for (int j = 1; j < size - k; j++)
-        {
-            if (arr[j] < arr[j - 1])
-            {
-                temp = arr[j];
+    {        x = 0;
+        for (int j = 1; j < size - k; j++)        {
+            if (arr[j] < arr[j - 1])            {                temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
-                x = 1;
-            }
-        }
-        if(x==0){break;}
-    }
+                x = 1;            }        }
+        if(x==0){break;}    }
 
     for (int y = 0; y < size; y++)
     {
--- lab9/3.c
+++ lab9/3.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 int MAXSIZE=10;
- 
 void main()
-{
-    float x[MAXSIZE];
+{   float x[MAXSIZE];
     int  i, n;
     float average, variance, std_deviation, sum = 0, sum1 = 0;
- 
     printf("Enter the value of N \n");
     scanf("%d", &n);
     printf("Enter %d real numbers \n", n);
     for (i = 0; i < n; i++)
-    {
-        scanf("%f", &x[i]);
-    }
- 
+    {       scanf("%f", &x[i]);   }
+     for (i = 0; i < n; i++)
+    {        sum+=x[i];
+    }   average = sum / (float)n;   
     for (i = 0; i < n; i++)
-    {
-        sum+=x[i];
-    }
-    average = sum / (float)n;
-    /*  Compute  variance  and standard deviation  */
-    for (i = 0; i < n; i++)
-    {
-        sum1 = sum1 + pow((x[i] - average), 2);
-    }
+    {       sum1 = sum1 + pow((x[i] - average), 2);    }
     variance = sum1 / (float)n;
     std_deviation = sqrt(variance);
     printf("Average of all elements = %.2f\n", average);
