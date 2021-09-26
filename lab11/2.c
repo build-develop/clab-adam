e7f4cd6efc2abb6ea0142fa611eb99c6121c3144
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-09-26 12:25:24 +0530

    11.3

--- lab11/2.c
+++ lab11/2.c
@@ -2,24 +2,33 @@
 #include <string.h>
 
 void main()
-{ int checker=0,count=0,maincount=0;
+{
+    int checker = 0, count = 0, maincount = 0;
     char x[100];
     printf("enter a sentence\t");
     gets(x);
-   char y[100];
+    char y[100];
     printf("Enter the word to search\t");
     gets(y);
 
-for(int i=0;i<strlen(x);i++){
-    if(x[i]==y[0]){checker=i;
-        for(int j=0;j<strlen(y);j++){
-            if(y[j]==x[checker++]){
-               count++; 
+    for (int i = 0; i < strlen(x); i++)
+    {
+        if (x[i] == y[0])
+        {
+            checker = i;
+            for (int j = 0; j < strlen(y); j++)
+            {
+                if (y[j] == x[checker++])
+                {
+                    count++;
+                }
+                if (count == (strlen(y) - 1))
+                {
+                    maincount++;
+                }
             }
-            if(count==(strlen(y)-1)){maincount++;}
+            count = 0;
         }
-        count=0;
     }
-}
-    printf("count: %ld",maincount);
+    printf("count: %ld", maincount);
 }
