d8c15ba343796fbb6aca406b03c3de42e480f8b1
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-09-26 12:12:42 +0530

    11.2

--- lab11/1.c
+++ lab11/1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
- #include <string.h>
- 
-void main(){
+#include <string.h>
+
+void main()
+{
     char x[100];
-    int count=0;
+    int count = 0;
     printf("enter a sentence\t");
     gets(x);
-for(int i=0;i<strlen(x);i++){
-if(x[i]==' '){
-    count++;
-}
-}
-printf("\nNumber of white spaces is: %ld",count);
+    for (int i = 0; i < strlen(x); i++)
+    {
+        if (x[i] == ' ')
+        {
+            count++;
+        }
+    }
+    printf("\nNumber of white spaces is: %ld", count);
 }
